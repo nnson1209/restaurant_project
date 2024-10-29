efefdc23bfa89f32d7ad5ecc3bc54c2256315916
Ho Ngoc Trung Quan <hntqun0805@gmail.com>
2024-10-30 00:36:28 +0700

    Update description of food

--- js/main.js
+++ js/main.js
@@ -117,3 +117,223 @@
     
 })(jQuery);
 
+// Food data
+const productData = [
+    {
+      id: 1,
+      imageUrl: "img/menu-1.jpg",
+      title: `
+        <h5 class="d-flex justify-content-between border-bottom py-2 mt-2 mt-lg-0 pt-lg-0 align-items-center">
+            <span class="text-start">Chicken Burger</span>
+            <span class="text-primary">$115</span>
+        </h5>`,
+      description:`
+        <small class="text-start d-flex flex-column">
+            <p>
+                Ingredients:<br>
+                &ensp;+ Chicken<br>
+                &ensp;+ Hamburger bread<br>
+                &ensp;+ Unsweetened fresh milk<br>
+                &ensp;+ Lemon juice<br>
+                &ensp;+ Vegetables
+            </p>
+            <p>
+                A chicken burger makes for a quick midweek meal or weekend BBQ’s with friends! Crispy seasoned chicken breast, topped with mandatory melted cheese, piled onto soft rolls with onion, avocado, lettuce, tomato and garlic mayo.
+            </p>
+        </small>`
+    },
+    {
+        id: 2,
+        imageUrl: "img/menu-2.jpg",
+        title: `
+          <h5 class="d-flex justify-content-between border-bottom py-2 mt-2 mt-lg-0 pt-lg-0 align-items-center">
+              <span class="text-start">Chicken Burger</span>
+              <span class="text-primary">$115</span>
+          </h5>`,
+        description:`
+          <small class="text-start d-flex flex-column">
+              <p>
+                  Ingredients:<br>
+                  &ensp;+ Chicken<br>
+                  &ensp;+ Hamburger bread<br>
+                  &ensp;+ Unsweetened fresh milk<br>
+                  &ensp;+ Lemon juice<br>
+                  &ensp;+ Vegetables
+              </p>
+              <p>
+                  A chicken burger makes for a quick midweek meal or weekend BBQ’s with friends! Crispy seasoned chicken breast, topped with mandatory melted cheese, piled onto soft rolls with onion, avocado, lettuce, tomato and garlic mayo.
+              </p>
+          </small>`
+    },
+    {
+        id: 3,
+        imageUrl: "img/menu-3.jpg",
+        title: `
+          <h5 class="d-flex justify-content-between border-bottom py-2 mt-2 mt-lg-0 pt-lg-0 align-items-center">
+              <span class="text-start">Chicken Burger</span>
+              <span class="text-primary">$115</span>
+          </h5>`,
+        description:`
+          <small class="text-start d-flex flex-column">
+              <p>
+                  Ingredients:<br>
+                  &ensp;+ Chicken<br>
+                  &ensp;+ Hamburger bread<br>
+                  &ensp;+ Unsweetened fresh milk<br>
+                  &ensp;+ Lemon juice<br>
+                  &ensp;+ Vegetables
+              </p>
+              <p>
+                  A chicken burger makes for a quick midweek meal or weekend BBQ’s with friends! Crispy seasoned chicken breast, topped with mandatory melted cheese, piled onto soft rolls with onion, avocado, lettuce, tomato and garlic mayo.
+              </p>
+          </small>`
+    },
+    {
+        id: 4,
+        imageUrl: "img/menu-4.jpg",
+        title: `
+          <h5 class="d-flex justify-content-between border-bottom py-2 mt-2 mt-lg-0 pt-lg-0 align-items-center">
+              <span class="text-start">Chicken Burger</span>
+              <span class="text-primary">$115</span>
+          </h5>`,
+        description:`
+          <small class="text-start d-flex flex-column">
+              <p>
+                  Ingredients:<br>
+                  &ensp;+ Chicken<br>
+                  &ensp;+ Hamburger bread<br>
+                  &ensp;+ Unsweetened fresh milk<br>
+                  &ensp;+ Lemon juice<br>
+                  &ensp;+ Vegetables
+              </p>
+              <p>
+                  A chicken burger makes for a quick midweek meal or weekend BBQ’s with friends! Crispy seasoned chicken breast, topped with mandatory melted cheese, piled onto soft rolls with onion, avocado, lettuce, tomato and garlic mayo.
+              </p>
+          </small>`
+    },
+    {
+        id: 5,
+        imageUrl: "img/menu-5.jpg",
+        title: `
+          <h5 class="d-flex justify-content-between border-bottom py-2 mt-2 mt-lg-0 pt-lg-0 align-items-center">
+              <span class="text-start">Chicken Burger</span>
+              <span class="text-primary">$115</span>
+          </h5>`,
+        description:`
+          <small class="text-start d-flex flex-column">
+              <p>
+                  Ingredients:<br>
+                  &ensp;+ Chicken<br>
+                  &ensp;+ Hamburger bread<br>
+                  &ensp;+ Unsweetened fresh milk<br>
+                  &ensp;+ Lemon juice<br>
+                  &ensp;+ Vegetables
+              </p>
+              <p>
+                  A chicken burger makes for a quick midweek meal or weekend BBQ’s with friends! Crispy seasoned chicken breast, topped with mandatory melted cheese, piled onto soft rolls with onion, avocado, lettuce, tomato and garlic mayo.
+              </p>
+          </small>`
+    },
+    {
+        id: 6,
+        imageUrl: "img/menu-6.jpg",
+        title: `
+          <h5 class="d-flex justify-content-between border-bottom py-2 mt-2 mt-lg-0 pt-lg-0 align-items-center">
+              <span class="text-start">Chicken Burger</span>
+              <span class="text-primary">$115</span>
+          </h5>`,
+        description:`
+          <small class="text-start d-flex flex-column">
+              <p>
+                  Ingredients:<br>
+                  &ensp;+ Chicken<br>
+                  &ensp;+ Hamburger bread<br>
+                  &ensp;+ Unsweetened fresh milk<br>
+                  &ensp;+ Lemon juice<br>
+                  &ensp;+ Vegetables
+              </p>
+              <p>
+                  A chicken burger makes for a quick midweek meal or weekend BBQ’s with friends! Crispy seasoned chicken breast, topped with mandatory melted cheese, piled onto soft rolls with onion, avocado, lettuce, tomato and garlic mayo.
+              </p>
+          </small>`
+    },
+    {
+        id: 7,
+        imageUrl: "img/menu-7.jpg",
+        title: `
+          <h5 class="d-flex justify-content-between border-bottom py-2 mt-2 mt-lg-0 pt-lg-0 align-items-center">
+              <span class="text-start">Chicken Burger</span>
+              <span class="text-primary">$115</span>
+          </h5>`,
+        description:`
+          <small class="text-start d-flex flex-column">
+              <p>
+                  Ingredients:<br>
+                  &ensp;+ Chicken<br>
+                  &ensp;+ Hamburger bread<br>
+                  &ensp;+ Unsweetened fresh milk<br>
+                  &ensp;+ Lemon juice<br>
+                  &ensp;+ Vegetables
+              </p>
+              <p>
+                  A chicken burger makes for a quick midweek meal or weekend BBQ’s with friends! Crispy seasoned chicken breast, topped with mandatory melted cheese, piled onto soft rolls with onion, avocado, lettuce, tomato and garlic mayo.
+              </p>
+          </small>`
+    },
+    {
+        id: 8,
+        imageUrl: "img/menu-8.jpg",
+        title: `
+          <h5 class="d-flex justify-content-between border-bottom py-2 mt-2 mt-lg-0 pt-lg-0 align-items-center">
+              <span class="text-start">Chicken Burger</span>
+              <span class="text-primary">$115</span>
+          </h5>`,
+        description:`
+          <small class="text-start d-flex flex-column">
+              <p>
+                  Ingredients:<br>
+                  &ensp;+ Chicken<br>
+                  &ensp;+ Hamburger bread<br>
+                  &ensp;+ Unsweetened fresh milk<br>
+                  &ensp;+ Lemon juice<br>
+                  &ensp;+ Vegetables
+              </p>
+              <p>
+                  A chicken burger makes for a quick midweek meal or weekend BBQ’s with friends! Crispy seasoned chicken breast, topped with mandatory melted cheese, piled onto soft rolls with onion, avocado, lettuce, tomato and garlic mayo.
+              </p>
+          </small>`
+    }
+  ];
+  
+  // Show basing on ID
+  function showDescription(productId) {
+    const product = productData.find(p => p.id === productId);
+  
+    if (product) {
+      document.getElementById("productImage").src = product.imageUrl;
+      document.getElementById("productTitle").innerHTML = product.title;
+      document.getElementById("productDescription").innerHTML = product.description;
+  
+      const descriptionBox = document.getElementById("descriptionBox");
+      const overlay = document.getElementById("overlay");
+
+      descriptionBox.classList.add("show");
+      overlay.classList.add("show");
+      document.body.classList.add("no-scroll");
+    }
+  }
+  
+  // Close description
+  function closeDescription() {
+    const descriptionBox = document.getElementById("descriptionBox");
+    const overlay = document.getElementById("overlay");
+
+    descriptionBox.classList.remove("show");
+    overlay.classList.remove("show");
+    document.body.classList.remove("no-scroll");
+  }
+
+  function addToCart() {
+    alert("Product has been added to the cart!");
+    closeDescription();
+  }
